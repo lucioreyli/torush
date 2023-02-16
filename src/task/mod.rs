822711bc8f7913b1689eb11f8c99117b67237e0b
lucioroadtoglory <lucioandradejr@gmail.com>
2023-02-16 09:58:28 -0300

    feat: finish list tasks

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -1,7 +1,10 @@
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Task {
+    pub id: u16,
     pub name: String,
     pub completed: bool,
+    pub created_at: DateTime<Utc>,
 }
